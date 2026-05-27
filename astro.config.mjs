643dbef89cb6ff1b78b1f829951663a4d40ff513
Whirling <infernity@infernity.dev>
2026-05-27 16:40:59 -0600

    whirli.ng

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://whirl.accorp.net",
+    site: "https://whirli.ng",
     base: "/"
 });
